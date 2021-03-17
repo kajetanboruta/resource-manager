a592d81708491c1c12b888ec4ed4186ce0d1ae20
diablx <kajetanboruta@gmail.com>
2021-03-17 14:47:40 +0100

    added new tests

--- spec/controllers/api/v1/tags_controller_spec.rb
+++ spec/controllers/api/v1/tags_controller_spec.rb
@@ -5,33 +5,40 @@ RSpec.describe "Tags", type: :request do
         it "returns list of all tags" do
             tag1 = create(:tag, name: 'Tag 1')
             tag2 = create(:tag, name: 'Tag 2')
-            tag3 = create(:tag, name: 'Tag 3')
-            tag4 = create(:tag, name: 'Tag 4')
             
             get '/api/v1/tags'
            
-            expect(response).to have_http_status(200)  
+            expect(response).to have_http_status(:success)  
             
             json = JSON.parse(response.body) 
-
-            expect(json['data'].first['attributes']['name']).to eq "Tag 1"
-            expect(json['data'].count).to eq Tag.count
+            tags = json['data']
+                        
+            expect(tags[0]['type']).to eq "tags"
+            expect(tags[0]['links']['self']).to eq "http://www.example.com/api/v1/tags/#{tag1.id}"
+            expect(tags[0]['attributes']['name']).to eq "Tag 1"   
+            expect(tags[1]['type']).to eq "tags"
+            expect(tags[1]['links']['self']).to eq "http://www.example.com/api/v1/tags/#{tag2.id}"
+            expect(tags[1]['attributes']['name']).to eq "Tag 2"
+            expect(tags.count).to eq 2
         end
     end    
     describe "GET /tags/:id" do
         it "returns tag by id" do
             tag1 = create(:tag, name: 'Tag 1')
             
-            get '/api/v1/tags'
+            get "/api/v1/tags/#{tag1.id}"
         
-            expect(response).to have_http_status(200)
+            expect(response).to have_http_status(:success)
 
             json = JSON.parse(response.body)
-            expect(json['data'].first['id'].to_i).to eq tag1.id
+            binding.pry
+            expect(json['data']['id'].to_i).to eq tag1.id
         end
         context "when no tag_id matches given id" do
             it 'returns error' do
-                
+                get '/api/v1/tags/1'
+
+                expect(response).to have_http_status(:not_found)
             end
         end
     end
@@ -48,6 +55,9 @@ RSpec.describe "Tags", type: :request do
             }.stringify_keys.to_json
             
             post '/api/v1/tags', params: hash, headers: headers
+
+            expect(response).to have_http_status(:success)
+
             json = JSON.parse(response.body)
             
             expect(json['data']['attributes']['name']).to eq('ruby')
@@ -75,27 +85,47 @@ RSpec.describe "Tags", type: :request do
             put "/api/v1/tags/#{tag1.id}", params: hash, headers: headers
             
             json = JSON.parse(response.body)
-            binding.pry
             expect(json['data']['attributes']['name']).to eq('ruby_new')
         end
         context 'when no tag_id matches given id' do
             it 'returns error' do
+                headers = { 'ACCEPT' => 'application/vnd.api+json', 'CONTENT_TYPE' => 'application/vnd.api+json' }
+                hash = {
+                    data: {
+                        type: "tags",
+                        id: 1,
+                        attributes: {
+                            name: "ruby_new"
+                        }
+                    }
+                }.stringify_keys.to_json
+
+                put '/api/v1/tags/1', params: hash, headers: headers
                 
+                expect(response).to have_http_status(:not_found)
             end
         end
         context 'when no tag_category selected' do
             it 'returns error' do
+                
+
                 
             end
         end
     end
     describe "DELETE /tags/:id" do
         it "removes tag by id" do
+            tag1 = create(:tag, name: 'ruby')
+
+            delete "/api/v1/tags/#{tag1.id}"
             
+            expect(response).to have_http_status(:success)
         end
         context 'when no id matches given id' do
             it 'returns error' do
+                delete '/api/v1/tags/1'
                 
+                expect(response).to have_http_status(:not_found)
             end
         end
     end
