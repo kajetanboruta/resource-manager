9a4dc03c5294cb047a0b93c6f46fd303477aadf5
diablx <kajetanboruta@gmail.com>
2021-03-30 13:30:48 +0200

    added error handling in tests, fixes to some tests, fixed formatting in some files

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -2,4 +2,5 @@ class Tag < ApplicationRecord
   belongs_to :tag_category, optional: true
   has_many :tag_assignments
   validates :name, presence: true
+  validates :tag_category, on: :create, presence: true
 end
--- spec/controllers/api/v1/tags_controller_spec.rb
+++ spec/controllers/api/v1/tags_controller_spec.rb
@@ -6,7 +6,6 @@ RSpec.describe 'Tags', type: :request do
       tag_category1 = create(:tag_category, name: 'programming language')
       tag1 = create(:tag, name: 'Ruby', tag_category_id: tag_category1.id)
       tag2 = create(:tag, name: 'JS', tag_category: tag_category1)
-      
 
       get '/api/v1/tags'
 
@@ -26,7 +25,8 @@ RSpec.describe 'Tags', type: :request do
 
   describe 'GET /tags/:id' do
     it 'returns tag by id' do
-      tag1 = create(:tag, name: 'Ruby')
+      tag_category1 = create(:tag_category, name: 'programming language')
+      tag1 = create(:tag, name: 'Ruby', tag_category: tag_category1)
 
       get "/api/v1/tags/#{tag1.id}"
 
@@ -39,14 +39,18 @@ RSpec.describe 'Tags', type: :request do
     context 'when no tag_id matches given id' do
       it 'returns error' do
         get '/api/v1/tags/1'
-
+        
+        json = JSON.parse(response.body)
+        errors = json.fetch('errors')
         expect(response).to have_http_status(:not_found)
+        expect(errors[0]['status']).to eq('404')
+        expect(errors[0]['detail']).to eq("The record identified by 1 could not be found.")
       end
     end
   end
 
   describe 'POST /tags' do
-    it 'creates new tag' do
+    it 'creates new tag by given attributes and returns persisted object' do
       headers = { 'ACCEPT' => 'application/vnd.api+json', 'CONTENT_TYPE' => 'application/vnd.api+json' }
       tag_category1 = create(:tag_category, name: 'programming language')
       hash = {
@@ -69,22 +73,37 @@ RSpec.describe 'Tags', type: :request do
       post '/api/v1/tags', params: hash, headers: headers
 
       expect(response).to have_http_status(:success)
-
       json = JSON.parse(response.body)
-
       expect(json['data']['attributes']['name']).to eq('ruby')
     end
 
-    context 'when no tag_category selected' do
-      it 'returns error' do
+    context 'when tag category parameters is not provided' do
+      it 'returns 422 http code, unprocessable_entity' do
+        headers = { 'ACCEPT' => 'application/vnd.api+json', 'CONTENT_TYPE' => 'application/vnd.api+json' }
+        hash = {
+          data: {
+            type: 'tags',
+            attributes: {
+              name: 'ruby'
+            }
+          }
+        }.stringify_keys.to_json
+
+        post '/api/v1/tags', params: hash, headers: headers
+
+        json = JSON.parse(response.body)
+        errors = json.fetch('errors')
+        expect(response).to have_http_status(:unprocessable_entity)
+        expect(errors[0]['status']).to eq('422')
+        expect(errors[0]['detail']).to eq("tag-category - can't be blank")
       end
     end
   end
 
   describe 'PUT /tags/:id' do
     it 'updates tag by id' do
-      tag_category1 = create(:tag_category, name: 'programming language')
-      tag1 = create(:tag, name: 'ruby')
+      tag_category_good = create(:tag_category, name: 'programming language')
+      tag1 = create(:tag, name: 'ruby', tag_category: tag_category_good)
       headers = { 'ACCEPT' => 'application/vnd.api+json', 'CONTENT_TYPE' => 'application/vnd.api+json' }
       hash = {
         data: {
@@ -97,14 +116,16 @@ RSpec.describe 'Tags', type: :request do
             'tag-category': {
               data: {
                 type: 'tag-categories',
-                id: tag_category1.id
+                id: tag_category_good.id
               }
             }
           }
         }
       }.stringify_keys.to_json
 
-      put "/api/v1/tags/#{tag1.id}", params: hash, headers: headers
+      expect { put "/api/v1/tags/#{tag1.id}", params: hash, headers: headers }.to change {
+                                                                                    tag1.reload.name
+                                                                                  }.from('ruby').to('ruby_new')
 
       json = JSON.parse(response.body)
       expect(json['data']['attributes']['name']).to eq('ruby_new')
@@ -129,7 +150,7 @@ RSpec.describe 'Tags', type: :request do
       end
     end
 
-    context 'when no tag_category selected' do
+    context 'when no tag_category is selected' do
       it 'returns error' do
       end
     end
@@ -137,7 +158,8 @@ RSpec.describe 'Tags', type: :request do
 
   describe 'DELETE /tags/:id' do
     it 'removes tag by id' do
-      tag1 = create(:tag, name: 'ruby')
+      tag_category1 = create(:tag_category, name: 'programming language')
+      tag1 = create(:tag, name: 'ruby', tag_category: tag_category1)
 
       delete "/api/v1/tags/#{tag1.id}"
 
@@ -145,11 +167,14 @@ RSpec.describe 'Tags', type: :request do
     end
 
     context 'when no id matches given id' do
-      it 'returns error' do
-
+      it 'returns 404 http code' do
         delete '/api/v1/tags/1'
 
+        json = JSON.parse(response.body)
+        errors = json.fetch('errors')
         expect(response).to have_http_status(:not_found)
+        expect(errors[0]['status']).to eq('404')
+        expect(errors[0]['detail']).to eq("The record identified by 1 could not be found.")
       end
     end
   end
